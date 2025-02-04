a9dc14f267d955c388c7d602d930c21514cf948d
ROHIT HALDER <141175246+rhRohitHalder@users.noreply.github.com>
2025-02-05 00:27:24 +0530

    Time: 0 ms (100%), Space: 28.3 MB (90.48%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -4,12 +4,14 @@ public:
         int n = nums.size();
         vector<int> ans(n,-1);
         stack<int> st;
-        for(int i=0 ;i < n*2 ;i++){
-           while(!st.empty() && nums[st.top()] < nums[i%n]){
-            ans[st.top()] = nums[i%n];
-            st.pop();
-           }
-           if(i<n) st.push(i);
+        for(int i=0; i<n*2; ++i){
+            int ele = nums[i];
+            while(!st.empty() && nums[st.top()] < nums[i%n] ){
+                ans[st.top()] = nums[i%n];
+                st.pop();
+            }
+
+            if(i<n) st.push(i); // input in stack
         }
         return ans;
     }
