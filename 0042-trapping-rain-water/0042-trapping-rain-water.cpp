d855419eb1b0092fc42386aae024501ed9aa0b98
ROHIT HALDER <141175246+rhRohitHalder@users.noreply.github.com>
2025-02-18 01:49:23 +0530

    Time: 0 ms (100%), Space: 28.2 MB (5.16%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,36 +1,31 @@
 class Solution {
 public:
-    vector<int> suffMax(vector<int> &arr){
-        int n = arr.size();
-        vector<int> ans(n);
-        ans[n-1] = arr[n-1];
-        for(int i=n-2;i>=0;--i){
-           ans[i] = max(ans[i+1] , arr[i]);
-        }
-        return ans;
-    }
-    vector<int> prefMax(vector<int> &arr){
+    vector<int> prefixMax(vector<int>arr){
         int n = arr.size();
         vector<int> ans(n);
         ans[0] = arr[0];
-        for(int i=1 ;i < n ;i++){
-           ans[i] = max(ans[i-1] , arr[i]);
+        for(int i=1;i<n;i++){
+            ans[i] = max(ans[i-1], arr[i]);
+        }
+        return ans;
+    }
+    vector<int> suffixMax(vector<int>arr){
+        int n = arr.size();
+        vector<int> ans(n);
+        ans[n-1] = arr[n-1];
+        for(int i=n-2;i>=0;i--){
+            ans[i] = max(ans[i+1], arr[i]);
         }
         return ans;
     }
     int trap(vector<int>& height) {
         int n = height.size();
-        if(n==0) return 0;
-
-        vector<int> suffix_Max = suffMax(height);
-        vector<int> prefix_Max = prefMax(height);
+        vector<int> Lmax = prefixMax(height);
+        vector<int> Rmax = suffixMax(height);
         int total = 0;
-        for(int i=0;i<n;i++)
-        {
-            if((suffix_Max[i] > height[i]) && (prefix_Max[i] > height[i])){
-                total += min(suffix_Max[i] , prefix_Max[i]) - height[i];
-            }
+        for(int i=0;i<n;i++ ){
+            total += min(Lmax[i],Rmax[i]) - height[i];
         }
-        return total;
+return total;
     }
 };
