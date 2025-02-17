ec516a65961be3303beb5c1a9efef8737c338fb7
ROHIT HALDER <141175246+rhRohitHalder@users.noreply.github.com>
2025-02-18 01:57:18 +0530

    Time: 0 ms (100%), Space: 25.8 MB (93.78%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,31 +1,28 @@
 class Solution {
 public:
-    vector<int> prefixMax(vector<int>arr){
-        int n = arr.size();
-        vector<int> ans(n);
-        ans[0] = arr[0];
-        for(int i=1;i<n;i++){
-            ans[i] = max(ans[i-1], arr[i]);
-        }
-        return ans;
-    }
-    vector<int> suffixMax(vector<int>arr){
-        int n = arr.size();
-        vector<int> ans(n);
-        ans[n-1] = arr[n-1];
-        for(int i=n-2;i>=0;i--){
-            ans[i] = max(ans[i+1], arr[i]);
-        }
-        return ans;
-    }
     int trap(vector<int>& height) {
         int n = height.size();
-        vector<int> Lmax = prefixMax(height);
-        vector<int> Rmax = suffixMax(height);
-        int total = 0;
-        for(int i=0;i<n;i++ ){
-            total += min(Lmax[i],Rmax[i]) - height[i];
+        int Lmax = height[0];
+        int Rmax = height[n-1];
+        int l = 1 , r = n-2;
+        int total = 0 ;
+        while(l<=r){
+            if(Lmax < Rmax){
+                if(height[l] <= Lmax ){
+                    total += (Lmax-height[l]);
+                }else{
+                    Lmax = height[l] ;
+                }
+                l++;
+            } else{
+                if(height[r] < Rmax ){
+                    total += (Rmax-height[r]);
+                }else{
+                    Rmax = height[r] ;
+                }
+                r--;
+            }
         }
-return total;
+        return total;
     }
 };
