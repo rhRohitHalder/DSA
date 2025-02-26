b507c312244ba0a5a19d1a765577378f5c06b7bd
ROHIT HALDER <141175246+rhRohitHalder@users.noreply.github.com>
2025-02-27 01:49:20 +0530

    Time: 6 ms (30.29%), Space: 11.8 MB (28.56%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.cpp
+++ 0402-remove-k-digits/0402-remove-k-digits.cpp
@@ -1,21 +1,36 @@
 class Solution {
 public:
-        string removeKdigits(string num, int k) {
-        string ans="";
-        for(char &c:num)
-        {
-            while(ans.size() && ans.back()>c &&k)
-            {
-                ans.pop_back();
+    string removeKdigits(string num, int k) {
+        if(num.length() == k) return "0";  // Edge case: Remove all digits
+        
+        stack<char> st;
+        for(char digit : num) {
+            while(!st.empty() && k > 0 && st.top() > digit) {
+                st.pop();  // Remove larger elements to form a smaller number
                 k--;
             }
-            if(ans.size()||c!='0')ans.push_back(c);
+            st.push(digit);
         }
-        while(ans.size()&&k--)         
-        //  <-------this look is to make sure we fulfil the deletion condition
-        {
-            ans.pop_back();
+        
+        // Remove remaining k digits if needed
+        while(k > 0 && !st.empty()) {
+            st.pop();
+            k--;
         }
-        return (ans=="")?"0":ans;
+
+        // Build the final result
+        string result;
+        while(!st.empty()) {
+            result += st.top();
+            st.pop();
+        }
+        reverse(result.begin(), result.end());
+
+        // Remove leading zeros
+        int i = 0;
+        while(i < result.length() && result[i] == '0') i++;
+        result = result.substr(i);
+
+        return result.empty() ? "0" : result;
     }
 };
